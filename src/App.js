7bc57623827518935efe60a0fab4fb24aaf82139
h0ma-s <92868104+h0ma-s@users.noreply.github.com>
2022-03-25 03:18:05 +0430

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weaather App</h1>
+        <footer>
+          This project was coded by Homa Sharifmousavi and is{" "}
+          <a href="https://github.com/h0ma-s/react-weather-project">
+            open-sourced
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
