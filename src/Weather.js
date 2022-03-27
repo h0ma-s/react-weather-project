0dabe09ba52b5173227286eb91c33571ad9960f9
h0ma-s <92868104+h0ma-s@users.noreply.github.com>
2022-03-28 04:12:25 +0430

    Formatted the date

--- src/Weather.js
+++ src/Weather.js
@@ -1,27 +1,22 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
-export default function Weather() {
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ loaded: false });
   function handleResponse(response) {
-    console.log(response);
     setWeatherData({
+      loaded: true,
       temp: response.data.main.temp,
       city: response.data.name,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
+      date: new Date(response.data.dt * 1000),
     });
-    setLoaded(true);
   }
 
-  const [weatherData, setWeatherData] = useState({});
-  const [loaded, setLoaded] = useState(false);
-  const apiKey = "e595356bb77e874bab1cb87dc84b6d45";
-  let city = "San Francisco";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
-`;
-
-  if (loaded === true) {
+  if (weatherData.loaded) {
     return (
       <div className="Weather">
         <form>
@@ -38,8 +33,8 @@ export default function Weather() {
           </div>
         </form>
         <h1>{weatherData.city}</h1>
-        <div>{weatherData.description}</div>
-        <div>Saturday 06:00</div>
+        <div className="text-capitalize">{weatherData.description}</div>
+        <FormattedDate date={weatherData.date} />
         <img src="/media/02d.png" alt="Partly cloudy" />
         <h2>{Math.round(weatherData.temp)}°C</h2>
         <div className="mt-3">
@@ -50,6 +45,9 @@ export default function Weather() {
       </div>
     );
   } else {
+    const apiKey = "e595356bb77e874bab1cb87dc84b6d45";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric
+`;
     axios.get(apiUrl).then(handleResponse);
     return (
       <div className="Weather">
